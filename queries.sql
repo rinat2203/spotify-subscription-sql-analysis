e82b7cb7aa9797ffe99e954b0dbfbca915cd6ab5
Rinat <riboram@gmail.com>
2026-05-15 10:35:30 -0700

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,8 @@
+-- Spotify Subscription SQL Analysis
+-- Author: Rinat Zhanakhmetov
+-- Tools: MySQL
+
+-- Tables used:
+-- users(user_id, name, registration_date)
+-- subscriptions(subscription_id, user_id, plan_type, price, start_date, end_date)
+-- payments(payment_id, user_id, amount, payment_date)
